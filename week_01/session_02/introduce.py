617b4e5481cd4e25c24b273acae530b08488c4d8
Oyunbold <lapipo82@gmail.com>
2025-03-27 17:53:56 +0800

    add: session_02

---
+++ week_01/session_02/introduce.py
@@ -0,0 +1,10 @@
+# introduce myself -- this is comment
+# introduce yourself in three lines
+# for example your firstname, lastname, age, gender
+# and occupation and hobbies
+
+print("My name is Oyunbold Naranjargal")
+print("I'm 17 years old")
+print("I'm a man")
+print("I like FPS video games, football and cycling")
+print("I wrote it from Nano Editor")
